9a17910cae675c6e02c3b44f1e522828cfc1d7f2
Dev Tung <devtung27101992@gmail.com>
2026-07-05 12:34:30 +0700

    update code

--- app/modules/job/routes/web.php
+++ app/modules/job/routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
 // =========================
-// YONEX CRAWLER
+// SHOP PROCEDURE
 // =========================
 
 Router::get('/job/yonex-category-crawl', 'YonexCategoryCrawler@run', ['auth' => 'admin']);
 Router::get('/job/yonex-product-crawl', 'YonexProductCrawler@run', ['auth' => 'admin']);
 Router::get('/job/yonex-product-detail-crawl', 'YonexProductDetailCrawler@run', ['auth' => 'admin']);
 Router::get('/job/yonex-product-import', 'YonexProductImporter@run', ['auth' => 'admin']);
-
-// =========================
-// SHOP PROCEDURE
-// =========================
-
 Router::get('/job/shop-procedure-create', 'ShopProcedureCreator@run', ['auth' => 'admin']);
