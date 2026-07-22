ab134c75409e3fbe30f120654fb4487381c438b1
Dev Tung <devtung27101992@gmail.com>
2026-07-22 13:37:10 +0700

    update code git push

--- app/common/layouts/website/footer.php
+++ app/common/layouts/website/footer.php
@@ -272,7 +272,9 @@
         </div>
 
     </footer>
-
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    
     <?php if (View::js()): ?>
         <script type="module" src="<?= View::js() ?>"></script>
     <?php endif; ?>
